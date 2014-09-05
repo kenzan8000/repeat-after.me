d365c366df7beb4057652b81e1b8120707403080
kenzan8000 <kenzan8000@gmail.com>
2014-09-05 19:12:57 +0900

    initial commit

---
+++ app/app.rb
@@ -0,0 +1,29 @@
+require 'bundler'
+Bundler.require
+
+
+set :haml, {:format => :html5}
+enable :sessions, :logging
+
+
+#require_relative 'models/init'
+
+
+get '/' do
+  haml :index
+end
+
+#get "/js/*.js" do
+#  javascript :application
+#end
+#
+#get "/css/*.css" do
+#  css :application
+#end
+#
+#get '/auth/google/callback' do
+#  @auth = request.env['omniauth.auth']
+#  #params[:oauth_token]
+#  #params[:oauth_verifier]
+#  haml :index
+#end
---
+++ app/config.ru
@@ -0,0 +1,4 @@
+css_dir = "/public/css/"
+sass_dir = "/assets/css/"
+output_style = :expanded
+line_comments = false
