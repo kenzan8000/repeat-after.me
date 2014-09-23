e3547c2d517e00aeec6ca8d69f2e3c3a1d2ca1fe
kenzan8000 <kenzan8000@gmail.com>
2014-09-22 23:24:21 -0700

    add cocoapods and project base

---
+++ ios/repeat-after-me/Classes/RATabBarController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+
+/// Override TabBarController for repeat-after.me
+class RATabBarController: UITabBarController {
+    /// life cycle
+    override func loadView() {
+        super.loadView()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ios/repeat-after-me/Classes/Recording/RARecordingViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+
+/// Recording
+class RARecordingViewController: UIViewController {
+    /// life cycle
+    override func loadView() {
+        super.loadView()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
